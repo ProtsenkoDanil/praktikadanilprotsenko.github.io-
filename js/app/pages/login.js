74c3de34434f0abf2887564dc64e07454a763140
Danil Protsenko <166614015+ProtsenkoDanil@users.noreply.github.com>
2025-01-29 14:20:18 +0100

    Update login.js

--- js/app/pages/login.js
+++ js/app/pages/login.js
@@ -2,24 +2,21 @@ export const login = {
     data: function() {
         return {
             img: 1,
-            hs: 0,
-            parent: '',
-            practiceText: 'ПРАКТИКА'
+            parent: null
         };
     },
     mounted: function() {
-        console.log(this.practiceText);
-        this.img = this.randomIntFromInterval(1, 7);
-        this.parent = this.$parent?.$parent || null;
+        this.parent = this.$parent?.$parent;
     },
     methods: {
-        randomIntFromInterval(min, max) {
-            return Math.floor(Math.random() * (max - min + 1) + min);
+        exampleMethod() {
+            console.log("Example method works!");
         }
     },
     template: `
-        <div class="flex">
-            <p>{{ practiceText }}</p> <!-- Отображаем текст "ПРАКТИКА" -->
+        <div>
+            <h1>Login Page</h1>
+            <p>Welcome to the login page</p>
         </div>
     `
 };
